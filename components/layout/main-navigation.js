6f3a4c176627643e55562eea5e0e8f6f60d8cf2a
dpak1999 <deepakkumardash313@gmail.com>
2021-04-23 00:01:11 +0530

    add : logo and stylin nav bar

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,25 +1,29 @@
 /** @format */
 import Link from "next/link";
+import Logo from "./logo";
+import classes from "./main-navigation.module.css";
 
 function MainNavigation() {
-  <header>
-    <Link href="/">
-      <a>
-        <Logo />
-      </a>
-    </Link>
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
 
-    <nav>
-      <ul>
-        <li>
-          <Link href="/posts">Post</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>;
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Post</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default MainNavigation;
