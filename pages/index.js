81cc4b11ae818e480379ab56a1d6040315a13bd2
dpak1999 <deepakkumardash313@gmail.com>
2021-04-25 23:58:46 +0530

    add : dummy posts

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,38 @@ import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
 
+const DUMMY_POSTS = [
+  {
+    title: "welcome the choice is either to repel the provision blind fault",
+    image: "bg1.jpg",
+    excerpt:
+      "and takes it takes to find and easy to follow accepted discomfort may be just about all of us is the result of an architect",
+    date: "2022-02-10",
+    slug: "getting-started",
+  },
+  {
+    title: "And it is blinding",
+    image: "bg.jpg",
+    excerpt:
+      "and takes it takes to find and easy to follow accepted discomfort may be just about all of us is the result of an architect",
+    date: "2022-02-10",
+    slug: "getting-started-1",
+  },
+  {
+    title: "great pain to open for them",
+    image: "my-life-through-a-lens",
+    excerpt:
+      "and takes it takes to find and easy to follow accepted discomfort may be just about all of us is the result of an architect",
+    date: "2022-02-10",
+    slug: "getting-started-2",
+  },
+];
+
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
