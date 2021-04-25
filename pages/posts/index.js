b5eeb2f0a603a379850dae488ee91a5c24bc3063
dpak1999 <deepakkumardash313@gmail.com>
2021-04-26 00:14:34 +0530

    add : all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,36 @@
 /** @format */
 
-function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    title: "welcome the choice is either to repel the provision blind fault",
+    image: "bg1.jpg",
+    excerpt:
+      "and takes it takes to find and easy to follow accepted discomfort may be just about all of us is the result of an architect",
+    date: "2022-02-10",
+    slug: "getting-started",
+  },
+  {
+    title: "And it is blinding",
+    image: "bg.jpg",
+    excerpt:
+      "and takes it takes to find and easy to follow accepted discomfort may be just about all of us is the result of an architect",
+    date: "2022-02-10",
+    slug: "getting-started-1",
+  },
+  {
+    title: "great pain to open for them",
+    image: "my-life-through-a-lens.jpg",
+    excerpt:
+      "and takes it takes to find and easy to follow accepted discomfort may be just about all of us is the result of an architect",
+    date: "2022-02-10",
+    slug: "getting-started-2",
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
