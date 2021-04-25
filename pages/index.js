af88a3ffda3254734ef75464e3f6ead2a8981240
dpak1999 <deepakkumardash313@gmail.com>
2021-04-26 00:04:32 +0530

    render dummy posts

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ const DUMMY_POSTS = [
   },
   {
     title: "great pain to open for them",
-    image: "my-life-through-a-lens",
+    image: "my-life-through-a-lens.jpg",
     excerpt:
       "and takes it takes to find and easy to follow accepted discomfort may be just about all of us is the result of an architect",
     date: "2022-02-10",
